8d5492af767922451c153dd4fc034d994d97b388
loket <loket@cruftlab.io>
2017-01-15 10:30:25 +0100

    updated index, show simple random word

---
+++ src/main/resources/static/script/app.js
@@ -0,0 +1,50 @@
+
+function getWord(url) {
+    axios.get(url)
+        .then(function (response) {
+            console.log("ok");
+            console.log(response);
+            console.log("Word: " + response.data.combinedForm);
+            word = response.data.combinedForm;
+        })
+        .catch(function (error) {
+            console.log("error");
+            console.log(error);
+        });
+     return word;
+};
+
+var Word = React.createClass({
+    loadRandomWord: function(url) {
+        axios
+            .get(url)
+            .then(function (response) {
+                this.setState({
+                    word: response.data.combinedForm
+                });
+            })
+    },
+
+    getInitialState: function() {
+        return {word: ""};
+    },
+
+    componentDidMount: function() {
+        var _this = this;
+        axios
+            .get(_this.props.url)
+            .then(function (response) {
+                _this.setState({
+                    word: response.data.combinedForm
+                });
+            })
+    },
+
+    render: function() {
+        return (
+            <div id="word"><h1>{this.state.word}</h1></div>
+        );
+    }
+});
+
+ReactDOM.render(<Word url="/api/words/random/2" />, document.getElementById('content'));
